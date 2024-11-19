8631bca475117c9457d6e32541bad5f6ded10fea
wrym1 <pbs1012004@gmail.com>
2024-11-19 15:46:34 +0200

    Update Project.cs

--- Project.cs
+++ Project.cs
@@ -1,1 +1,16 @@
 
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        GreetUser("User");
+    }
+
+    static void GreetUser(string name)
+    {
+        Console.WriteLine($"Hello, {name}!");
+    }
+}
